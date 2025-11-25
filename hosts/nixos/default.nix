6ca48254e7330999b62419ec696fa2de88a932e4
davalbagr <davidaagra@gmail.com>
2025-11-25 10:28:45 +0000

    update

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -123,6 +123,17 @@
     };
     libinput.touchpad.naturalScrolling = true;
     libinput.mouse.naturalScrolling = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        START_CHARGE_THRESH_BAT0 = 40;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+      };
+    };
   };
 
   home-manager = {
