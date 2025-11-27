5603bfa9cea51175b20afe9560f01fdaf697df3f
davalbagr <davidaagra@gmail.com>
2025-11-27 20:59:21 +0000

    update

--- os/linux/default.nix
+++ os/linux/default.nix
@@ -13,7 +13,31 @@
     auto-optimise-store = true;
   };
 
-  hardware.bluetooth.enable = true;
+  boot.extraModprobeConfig = ''
+    options iwlwifi bt_coex_active=0
+    options iwlwifi swcrypto=1
+    options iwlwifi power_save=0
+    options iwlwifi uapsd_disable=1
+    options iwlwifi d0i3_disable=1
+    options iwlmvm power_scheme=1
+  '';
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        ControllerMode = "bredr";
+        Experimental = true;
+        FastConnectable = false;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+
+  services.blueman.enable = true;
 
   boot.loader = {
     systemd-boot.enable = true;
