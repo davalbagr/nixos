a3429f9e4e4e33dfe48d6c98d7d8619ed31cb82a
davalbagr <davidaagra@gmail.com>
2025-11-30 17:51:44 +0000

    update

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -1,3 +1,16 @@
 _: {
   enable = true;
+  vencord = {
+    useSystem = true;
+    settings = {
+      autoUpdate = false;
+      autoUpdateNotification = false;
+      notifyAboutUpdates = true;
+      useQuickCss = true;
+      disableMinSize = true;
+      frameless = true;
+      transparent = true;
+      enabledThemes = ["rose-pine.css"];
+    };
+  };
 }
