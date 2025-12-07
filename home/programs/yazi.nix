06061b50efa853fefecfebba202f141e4c79d2e1
davalbagr <davidaagra@gmail.com>
2025-12-07 17:36:40 +0000

    update

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -1,3 +1,15 @@
-_: {
+{ pkgs, ... }:
+{
   enable = true;
+  enableFishIntegration = true;
+  flavors.rose-pine = pkgs.fetchFromGitHub {
+    owner = "Mintass";
+    repo = "rose-pine.yazi";
+    rev = "834334048d0bc8c7c344a5a8abce0b0cb35612d3";
+    hash = "sha256-eDJ0CsLK0ED0zl3MGfd1JtVcmwDHlkaOnliKltNneOo=";
+  };
+  settings.flavor = {
+    dark = "rose-pine";
+    light = "rose-pine";
+  };
 }
