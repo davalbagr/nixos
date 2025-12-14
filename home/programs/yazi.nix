0a7c6220a688e406bdb9928c58984863d28d18e0
davalbagr <davidaagra@gmail.com>
2025-12-14 12:19:15 +0000

    update

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -1,15 +1,26 @@
 { pkgs, ... }:
+let
+  rosePineTheme = pkgs.fetchFromGitHub {
+    owner = "rose-pine";
+    repo = "yazi";
+    rev = "fd385266af5f3419657e449607f3e87f062d0d2e";
+    hash = "sha256-3j7TTtzG+GCB4uVeCiuvb/0dCkHPz7X+MDBVVUp646A=";
+  };
+
+in
 {
   enable = true;
   enableBashIntegration = true;
-  flavors.rose-pine = pkgs.fetchFromGitHub {
-    owner = "Mintass";
-    repo = "rose-pine.yazi";
-    rev = "834334048d0bc8c7c344a5a8abce0b0cb35612d3";
-    hash = "sha256-eDJ0CsLK0ED0zl3MGfd1JtVcmwDHlkaOnliKltNneOo=";
+  flavors.rose-pine = "${rosePineTheme}/flavors/rose-pine.yazi";
+  settings = {
+    mgr = {
+      show_hidden = true;
+    };
   };
-  settings.flavor = {
-    dark = "rose-pine";
-    light = "rose-pine";
+  theme = builtins.fromTOML (builtins.readFile "${rosePineTheme}/themes/rose-pine.toml") // {
+    flavor = {
+      dark = "rose-pine";
+      light = "rose-pine";
+    };
   };
 }
