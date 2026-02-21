3040ce4b816a1146b814109e3400a8885bf6f666
davalbagr <davidaagra@gmail.com>
2026-02-21 17:32:58 +0000

    update

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -13,7 +13,8 @@ _: {
     lzd = "lazydocker";
     hrc = "heroku run console";
     n = "nvim";
-    oc = "opencode";
+    oc = "nix shell nixpkgs#nodejs -c bash -lc \"opencode\"";
+    ocs = "nix shell nixpkgs#nodejs -c bash -lc \"opencode serve\"";
     ls = "eza -lh --group-directories-first --icons";
     wl-fzf = "cliphist list | fuzzel --dmenu --with-nth 2 | cliphist decode";
     yz = "yazi";
